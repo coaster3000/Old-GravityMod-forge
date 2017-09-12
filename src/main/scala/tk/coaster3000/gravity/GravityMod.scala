c814ea356585b4ab1d7ebc052a34338976882cb7
Christopher Krier <ckrier.3000@gmail.com>
2017-09-11 21:50:26 -0400

    Initial setup + Mod Object

---
+++ src/main/scala/tk/coaster3000/gravity/GravityMod.scala
@@ -0,0 +1,11 @@
+package tk.coaster3000.gravity
+
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+@Mod(modid = GravityMod.MODID, name = GravityMod.MODNAME, version = GravityMod.MODVERSION, modLanguage = "scala")
+object GravityMod {
+	final val MODID = "physicsmod"
+	final val MODNAME = "Physics Mod"
+	final val MODVERSION = "@mod_version@"
+}
