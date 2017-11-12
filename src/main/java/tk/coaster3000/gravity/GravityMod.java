5ce0d54fa16f6bfd9dad92ed9379cb2a509a3991
Christopher Krier <ckrier.3000@gmail.com>
2017-11-12 12:21:24 -0500

    Cleanup and remove some code temporarily.

--- src/main/java/tk/coaster3000/gravity/GravityMod.java
+++ src/main/java/tk/coaster3000/gravity/GravityMod.java
@@ -5,30 +5,29 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+import tk.coaster3000.gravity.util.PhysicsUtil;
 
 import static tk.coaster3000.gravity.GravityMod.*;
 
 @Mod(modid = MODID, name = MODNAME, version = MODVERSION)
 public class GravityMod {
-	public static final String MODID = "physicsmod";
-	public static final String MODNAME = "Physics Mod";
+	static final String MODID = "physicsmod";
+	static final String MODNAME = "Physics Mod";
 
-	public static final String MODVERSION = "@mod_version@";
+	static final String MODVERSION = "@mod_version@";
 
-	static GravityMod instance = null;
-
-	private static void setInstance(GravityMod instance) {
-		GravityMod.instance = instance;
-	}
-
-	public static GravityMod getInstance() {
-		return GravityMod.instance;
-	}
+	private static GravityMod instance = null;
 
 	private Logger logger;
 
+
+	public Logger getLogger() {
+		return logger;
+	}
+
 	@Mod.EventHandler
 	public void onPreInit(FMLPreInitializationEvent event) {
+		setInstance(this);
 		this.logger = event.getModLog();
 		logger.info("Pre-Initialization Completed.");
 	}
@@ -36,5 +35,13 @@ public class GravityMod {
 	@Mod.EventHandler
 	public void onInit(FMLInitializationEvent event) {
 		logger.info("Initialization Completed.");
+	}
+
+	public static GravityMod getInstance() {
+		return GravityMod.instance;
+	}
+
+	private static void setInstance(GravityMod instance) {
+		GravityMod.instance = instance;
 	}
 }
